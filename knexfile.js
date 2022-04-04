b19cf6bba8f0408bef100b7fe6e83d32836394ef
Monyuy Divine <dbootable@gmail.com>
2022-04-04 05:08:06 +0300

    knex config changes

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,8 @@ module.exports = {
    production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: { 
+    ssl: {
+      require: true, 
       rejectUnauthorized: false
        },
     migrations: {
