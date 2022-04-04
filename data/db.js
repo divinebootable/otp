134e54d5b375b5153923809f3a8af99db4d6b8fa
Monyuy Divine <dbootable@gmail.com>
2022-04-04 10:45:38 +0300

    knex config changes

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 
 const knexfile = require("../knexfile");
 
-const env = process.env.DB_ENVIRONMENT || "development";
+const env = process.env.NODE_ENV || "development";
 const configOptions = knexfile[env];
 
 module.exports = knex(configOptions);
