af176bb59f5110ee30b7b116eb21cb876f115331
Monyuy Divine <dbootable@gmail.com>
2022-04-12 20:39:13 +0300

    any changes

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connectionString: process.env.DATABASE_URL,
+    connectionString:process.env.DATABASE_URL,
     pool: { min: 0, max: 200 },
     ssl: { rejectUnauthorized: false },
     migrations: {
@@ -14,7 +14,7 @@ module.exports = {
 
   testing: {
     client: "pg",
-    connectionString: process.env.DATABASE_URL,
+    connectionString:process.env.DATABASE_URL,
     pool: { min: 0, max: 200 },
     ssl: {
       rejectUnauthorized: false },
@@ -26,7 +26,7 @@ module.exports = {
 
    production: {
     client: "pg",
-    connectionString: process.env.DATABASE_URL,
+    connectionString:process.env.DATABASE_URL,
     pool: { min: 0, max: 200 },
     ssl: { rejectUnauthorized: false },
     migrations: {
