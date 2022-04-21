ddb1eb44a4d3be58f7df527a248dc26cc8783998
Monyuy Divine <dbootable@gmail.com>
2022-04-21 08:57:14 +0300

    import db

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const { constants } = require("buffer");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(fileupload());
 app.use(cors());
 
 app.use(index);
