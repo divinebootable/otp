aaf54c452425d53d9c340439459b235a8e843dd2
Monyuy Divine <dbootable@gmail.com>
2022-04-21 08:27:25 +0300

    import db

--- src/app.js
+++ src/app.js
@@ -21,16 +21,29 @@ app.use(index);
 app.use("/api/", registerRoute);
 app.use("/api/", accountRoute);
 
-app.post('/api/upload', async (req, res) => {
+// app.post('/api/upload', async (req, res) => {
+// 	console.log(req.files)
+//     const {name, data} = req.files.pic;
+//     const { users, created_on } = req.body;
+//     if (name && data) {
+//         await db.insert({name: name, data: data, users:users,created_on:created_on}).into('userImage');
+//         res.sendStatus(200);
+//     } else {
+//         res.sendStatus(400);
+//     }
+// })
+
+app.post('/api/upload', (req, res)=>{
 	console.log(req.files)
     const {name, data} = req.files.pic;
     const { users, created_on } = req.body;
-    if (name && data) {
-        await db.insert({name: name, data: data, users:users,created_on:created_on}).into('userImage');
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(400);
-    }
+     db("userImage")
+    .insert({ name, data, users, created_on })
+    .returning("*")
+    .then((data) => {
+      res.senStatus(200);
+    })
+    .catch((err) => res.status(400).json({ Error: "bad request" }));
 })
 
 module.exports = app;
