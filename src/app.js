85f9eea3faa256d8735073b8b9ee722d48d1de1e
Monyuy Divine <dbootable@gmail.com>
2022-04-21 10:47:41 +0300

    file upload

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use("/api/", accountRoute);
 
 app.post('/api/upload', (req, res)=>{
 	 const {name, data} = req.files.pic;
-     const { users, name, data, created_on } = req.body;
+     const { users, created_on } = req.body;
       console.log(req.body)
      db("userImage")
     .insert({ name, data,users,created_on })
